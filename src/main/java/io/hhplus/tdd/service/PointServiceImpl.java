775d8b98602835aaeb51e33253d6e95fab741721
iamipro <iamipro@naver.com>
2025-05-21 23:01:31 +0900

    1. 포인트서비스 구현체 리팩토링 2. 유저 포인트 히스토리 조회 TDD 통해 구현

--- src/main/java/io/hhplus/tdd/service/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/service/PointServiceImpl.java
@@ -1,17 +1,23 @@
 package io.hhplus.tdd.service;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.PointHistory;
 import io.hhplus.tdd.point.UserPoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 public class PointServiceImpl implements PointService {
+    @Autowired
+    private UserPointTable userPointTable;
+    @Autowired
+    private PointHistoryTable pointHistoryTable;
+
     @Override
     public List<PointHistory> selectUserPointHistory(long id) {
-        return List.of();
+        return pointHistoryTable.selectAllByUserId(id);
     }
 
     @Override
@@ -26,7 +32,6 @@ public class PointServiceImpl implements PointService {
 
     @Override
     public UserPoint selectUserPoint(long id) {
-        UserPointTable userPointTable = new UserPointTable();
         return userPointTable.selectById(id);
     }
 }
--- src/test/java/io/hhplus/tdd/service/PointServiceImplTest.java
+++ src/test/java/io/hhplus/tdd/service/PointServiceImplTest.java
@@ -8,43 +8,23 @@ import io.hhplus.tdd.point.UserPoint;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PointServiceImplTest {
 
+    // 행동
     @Mock
     PointHistoryTable pointHistoryTable;
     @Mock
     UserPointTable userPointTable;
-
-    @InjectMocks PointServiceImpl pointService;
-
-    
-    @Test
-    @DisplayName("유저 포인트 히스토리")
-    void selectUserPointHistory() {
-        
-    }
-
-    @Test
-    @DisplayName("유저 포인트 충전")
-    void chargeUserPoint() {
-
-    }
-
-    @Test
-    @DisplayName("유저 포인트 사용")
-    void useUserPoint() {
-
-
-    }
-
+ 
     @Test
     @DisplayName("유저 포인트 조회")
     void selectUserPoint() {
@@ -65,8 +45,57 @@ class PointServiceImplTest {
         assertEquals(expectedUserPoint.point(), userPoint.point());
         assertEquals(expectedUserPoint.updateMillis(),  userPoint.updateMillis());
 
+    }
+
+    
+    @Test
+    @DisplayName("유저 포인트 히스토리")
+    void selectUserPointHistory() {
+        // given =>long id
+        long id = 1L;
+        long userId = 1L;
+        long amount = 1000L;
+        TransactionType type = TransactionType.CHARGE;
+        long updateMillis = 1652953000000L;
+        PointHistory initPointHistory = new PointHistory(
+                id,
+                userId,
+                amount,
+                type,
+                updateMillis
+        );
+
+        // when
+        when(pointHistoryTable.selectAllByUserId(userId)).thenReturn(List.of(initPointHistory));
+        List<PointHistory> pointHistories = pointHistoryTable.selectAllByUserId(userId);
+
+        // then
+        assertEquals(1, pointHistories.size());
+        assertEquals(initPointHistory.id(), pointHistories.get(0).id());
+        assertEquals(initPointHistory.userId(), pointHistories.get(0).userId());
+        assertEquals(initPointHistory.amount(), pointHistories.get(0).amount());
+        assertEquals(initPointHistory.type(), pointHistories.get(0).type());
+        assertEquals(initPointHistory.updateMillis(), pointHistories.get(0).updateMillis());
 
     }
+
+    @Test
+    @DisplayName("유저 포인트 충전")
+    void chargeUserPoint() {
+
+
+
+
+    }
+
+    @Test
+    @DisplayName("유저 포인트 사용")
+    void useUserPoint() {
+
+
+    }
+
+
     
     
     
